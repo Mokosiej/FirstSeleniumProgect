cfe6e7d2a43b27b27d989e3388c5b69cf82ed508
Tatsiana <mokosiejtatsiana@gmail.com>
2025-02-07 01:44:27 +0100

    Initial commit

--- src/test/java/com/ait/tests/FirstSeleniumTest.java
+++ src/test/java/com/ait/tests/FirstSeleniumTest.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.net.URL;
 import java.time.Duration;
 import java.util.List;
 
