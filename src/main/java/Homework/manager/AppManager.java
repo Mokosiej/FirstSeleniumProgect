43755913f7f0bd21ea0f491aa40d0aa5c60fcf41
Tatsiana <mokosiejtatsiana@gmail.com>
2025-03-02 12:53:22 +0100

    Initial commit

--- src/main/java/Homework/manager/AppManager.java
+++ src/main/java/Homework/manager/AppManager.java
@@ -3,19 +3,52 @@ package Homework.manager;
 import Homework.helpers.CartHelper;
 import Homework.helpers.NavigationHelper;
 import Homework.helpers.UserHelper;
+import Homework.utils.MyListener;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringDecorator;
+import org.openqa.selenium.support.events.WebDriverListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
 
 public class AppManager {
+
+    String browser;
     WebDriver driver;
-    private UserHelper user;
-    private CartHelper cart;
-    private NavigationHelper navigation;
+    public static SoftAssert softAssert = new SoftAssert();
+    Logger logger = LoggerFactory.getLogger(AppManager.class);
+
+    UserHelper user;
+    CartHelper cart;
+    NavigationHelper navigation;
+
+    public AppManager(String browser) {
+        this.browser=browser;
+    }
+
 
     public void init() {
-        driver = new ChromeDriver();
+        if (browser.equalsIgnoreCase("chrome")) {
+            driver = new ChromeDriver();
+            logger.info("Tests start in Chrome browser");
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            driver = new FirefoxDriver();
+            logger.info("Tests start in Firefox browser");
+        } else if (browser.equalsIgnoreCase("edge")) {
+            driver = new EdgeDriver();
+            logger.info("Tests start in Edge browser");
+        }
+
+
+        WebDriverListener listener = new MyListener();
+        driver = new EventFiringDecorator<>(listener).decorate(driver);
+
+        logger.info("Current url --> " + driver.getCurrentUrl());
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://demowebshop.tricentis.com");
         driver.manage().window().maximize();
--- src/test/java/Homework/TestBase.java
+++ src/test/java/Homework/TestBase.java
@@ -1,15 +1,41 @@
 package Homework;
 
 import Homework.manager.AppManager;
+import org.openqa.selenium.remote.Browser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.lang.reflect.Method;
 
 
 public class TestBase {
 
-    protected static final AppManager app = new AppManager();
+    protected static  AppManager app = new AppManager(
+            System.getProperty("browser", Browser.CHROME.browserName()));
 
-        @BeforeMethod
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
+    @BeforeMethod
+    public void startTest(Method method) {
+        logger.info("Start test " + method.getName());
+    }
+
+    @AfterMethod
+    public void stopTest(ITestResult result) {
+        if (result.isSuccess()) {
+            logger.info("PASSED: " + result.getMethod().getMethodName() );
+        } else {
+            logger.error("FAILED: " + result.getMethod().getMethodName());
+        }
+        logger.info("Stop test");
+        logger.info("**********************************************************");
+    }
+
+    @BeforeMethod
         public void setUp() {
             app.init();
         }
